578a1d03857fd8afa6d195d245a62c42f574a78a
Nick <works.person@gmail.com>
2021-09-16 19:54:05 +0300

    added redux wrapper in project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import React, { FC } from 'react';
 import type { AppProps } from 'next/app'
 import '../styles/global.scss';
+import {wrapper} from "../store";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
+const  WrappedApp: FC<AppProps> = ({Component, pageProps}) => (
+  <Component {...pageProps} />
+);
+
+export default wrapper.withRedux(WrappedApp);
